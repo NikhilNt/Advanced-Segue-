6e321fa8166075f929947944412c1a0d038076b0
Nikhil Tanappagol <nikhiltrans123@gmail.com>
2017-09-14 20:55:23 -0700

    first commit

--- AdvanceSegue/ViewController.swift
+++ AdvanceSegue/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+var active = 0
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 4
+        
+    }
+    
+  
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Row\(indexPath.row)"
+        return cell
+        
+    
+    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        active = indexPath.row
+        performSegue(withIdentifier: "Transfer", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
